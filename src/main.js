2ee7c48d0a44c97e9562e90b23781d8871105e35
AliceEngineer <alice_engineer@yeah.net>
2022-11-15 06:52:02 +0800

    commit
    Signed-off-by: AliceEngineer <alice_engineer@yeah.net>

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/global.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const vue = createApp(App)
+
+vue.use(store)
+vue.use(router)
+vue.use(ElementPlus)
+vue.mount('#app')
